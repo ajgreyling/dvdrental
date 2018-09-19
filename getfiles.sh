5148a8cfa3b29565b40243f87c6c0dd4ae47be6b
AJ Greyling <agreyling@mezzanineware.com>
2018-09-19 22:25:36 +0200

    updated to allow the process to run with getfiles.sh already in the local folder

--- getfiles.sh
+++ getfiles.sh
@@ -1,6 +1,6 @@
 git init
 git remote add origin -f https://github.com/ajgreyling/dvdrental
-git pull origin master
+git fetch --all
+git reset --hard origin/master
 export PGPASSWORD=mysecretpassword
 psql -U postgres -h localhost -w -p 2345 -f restore.sql
-
